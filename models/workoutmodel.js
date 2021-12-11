ba230b0b5abbd87ec41f0c3091bcbc9d79803150
James Sgarella <Jimbo8702@gmail.com>
2021-12-11 16:37:00 -0500

    some routes are working

---
+++ models/workoutmodel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: Date,
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: {
+        type: Number,
+        required: false,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const workout = require("./models/workout.js");
+const Workout = require("./models/workoutmodel");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
@@ -23,6 +23,57 @@ app.get("/stats", (req, res) => {
 });
 app.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+// get routes
+//api/workouts
+app.get("/api/workouts", (req, res) => {
+  Workout.find()
+
+    .then((dbwork) => {
+      res.json(dbwork);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+//api/workouts/range
+app.get("/api/workouts/range", (req, res) => {});
+// put route
+//api/workout/id
+app.put("/api/workouts/:id", (req, res) => {
+  var aggr = Workout.aggregate([
+    {
+      $match: {
+        _id: Number(req.params.id),
+      },
+    },
+    {
+      $addFields: {
+        exercise: req.body,
+      },
+    },
+  ]);
+  // aggr.options = { allowDiskUse: true };
+  aggr.exec(function (err, workout) {
+    if (err) res.send(err);
+    res.json(workout);
+  });
+});
+
+// post routes
+//adds exercise field to a workout
+// api/workouts
+
+app.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((workout) => {
+      console.log("Workout Created!");
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.listen(PORT, () => {
